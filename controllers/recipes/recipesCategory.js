27b1f6d66d9e3f5365c04c5a5119f94b1f61cc50
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-01 17:18:39 +0300

    Update recipesCategory.js

--- controllers/recipes/recipesCategory.js
+++ controllers/recipes/recipesCategory.js
@@ -2,14 +2,14 @@ const { Recipe } = require("../../models");
 
 const recipesCategory = async (req, res) => {
   const { category } = req.params;
- console.log(category);
+  console.log(category);
   // const result = await Recipe.aggregate([
   //   { $group: { _id: "$category", items: { $push: "$$ROOT" } } },
   //   { $project: { firstFour: { $slice: ["$items", 8] } } },
   //   { $limit: 13 }, // Optional limit to the number of categories to return
   // ]);
 
-  const result = await Recipe.findAll({ category: category });
+  const result = await Recipe.find({ category: category });
 
   res.status(201).json({
     status: "success",
@@ -20,9 +20,7 @@ const recipesCategory = async (req, res) => {
 
 module.exports = recipesCategory;
 
-  //   const { _id } = req.user;
-  //   const { page = 1, limit = 10, favorite = false } = req.query;
-  //   const skip = (page - 1) * limit;
-  //   const objSearch = favorite ? { owner: _id, favorite: true } : { owner: _id };
-
-
+//   const { _id } = req.user;
+//   const { page = 1, limit = 10, favorite = false } = req.query;
+//   const skip = (page - 1) * limit;
+//   const objSearch = favorite ? { owner: _id, favorite: true } : { owner: _id };
