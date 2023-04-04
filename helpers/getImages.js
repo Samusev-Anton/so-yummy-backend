67c3f2c8be1adbd9da89f3664aa2b5d4fe8ce420
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2023-04-05 01:22:53 +0300

    fixed KEY

--- helpers/getImages.js
+++ helpers/getImages.js
@@ -1,7 +1,7 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const { KEY_PIXABBAY: KEY } = process.env;
 
-const KEY = "30695074-d0d0e1da504e36119503c6783";
 const fetchApi = (name, page) => {
   const URL = `https://pixabay.com/api/?q=${name}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`;
   const response = fetch(URL)
