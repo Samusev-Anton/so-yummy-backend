c3fb51d36a42b97ee37472767f38d88e24367fbf
alejandro111555 <al.mirskoy@gmail.com>
2023-04-04 21:51:29 +0300

    getFavoritsRecipe

--- controllers/favorits/deleteFavoritsRecipe.js
+++ controllers/favorits/deleteFavoritsRecipe.js
@@ -1,18 +1,26 @@
 const { User, Recipe } = require("../../models");
 const { HttpError } = require("../../helpers");
+const { ObjectId } = require("mongodb");
 
 const deleteFavoritsRecipe = async (req, res, next) => {
   const { _id } = req.params;
   const recipe = req.params.favorite;
 
+  // const result = await User.findByIdAndRemove(_id, {
+  //   $pull: { recipe },
+  // });
+  // if (!result) {
+  //   throw new HttpError(404, `Not Found ID=${req.params.favorite}`);
+  // }
+
   const result = await User.findByIdAndRemove(_id, {
     $pull: {
-      favoritsRecipe: recipe,
+      favoritsRecipe: {
+        recipe,
+      },
     },
   });
-  //   if (!result) {
-  //     throw new HttpError(404, `Not Found ID=${req.params.favorite}`);
-  //   }
+  console.log(recipe);
   res.json({
     status: "success",
     code: 200,
