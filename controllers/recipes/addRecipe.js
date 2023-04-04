07aaee3cdfc6e423a65328f64d8ee9af0f23563b
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2023-04-05 01:12:47 +0300

    added images as a default

--- controllers/recipes/addRecipe.js
+++ controllers/recipes/addRecipe.js
@@ -1,16 +1,23 @@
 const { Unauthorized } = require("http-errors");
-const { User, Recipe } = require("../../models");
+const { Recipe } = require("../../models");
+const fetchApi = require("../../helpers/getImages");
 
 const addRecipe = async (req, res, next) => {
   const { _id } = req.user;
   if (!_id) {
     throw new Unauthorized(`Please authorize`);
   }
-  const { path: url } = req.file;
   const recipe = req.body;
-  recipe.thumb = req.file ? url : "";
-  recipe.owner = _id;
 
+  if (req.file) {
+    const { path: url } = req.file;
+    recipe.thumb = url;
+  } else {
+    fetchApi(recipe.title, 1).then(
+      (data) => (recipe.thumb = data.hits[0].pageURL)
+    );
+  }
+  recipe.owner = _id;
   const newRecipe = await Recipe.create(recipe);
 
   res.status(201).json({
