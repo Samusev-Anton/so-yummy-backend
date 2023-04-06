494d5938585738b382fb121a4f3b5f281b1a7e79
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-06 12:15:06 +0300

    update favorite

--- controllers/favorits/addToFavoritsRecipe.js
+++ controllers/favorits/addToFavoritsRecipe.js
@@ -1,9 +1,18 @@
 const { User } = require("../../models");
-const { HttpError } = require("../../helpers");
 
 const addRecipeToFavorits = async (req, res, next) => {
-  const { _id } = req.user;
+  const { _id, favoritsRecipe } = req.user;
   const recipe = req.params.favorite;
+
+  const existRecipe = favoritsRecipe.some((item) => item.toString() === recipe);
+
+  if (existRecipe) {
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Ingridient already exist",
+    });
+  }
 
   const newFavorits = await User.findByIdAndUpdate(_id, {
     $push: {
--- controllers/favorits/deleteFavoritsRecipe.js
+++ controllers/favorits/deleteFavoritsRecipe.js
@@ -1,26 +1,16 @@
 const { User } = require("../../models");
 // const { HttpError } = require("../../helpers");
-// const { ObjectId } = require("mongodb");
+const { ObjectId } = require("mongodb");
 
 const deleteFavoritsRecipe = async (req, res, next) => {
   const { _id } = req.user;
   const recipe = req.params.favorite;
 
-  // const result = await User.findByIdAndRemove(_id, {
-  //   $pull: { recipe },
-  // });
-  // if (!result) {
-  //   throw new HttpError(404, `Not Found ID=${req.params.favorite}`);
-  // }
+  const result = await User.updateOne(
+    { _id: ObjectId(_id) },
+    { $pull: { favoritsRecipe: recipe } }
+  );
 
-  const result = await User.findByIdAndRemove(_id, {
-    $pull: {
-      favoritsRecipe: {
-        recipe,
-      },
-    },
-  });
-  console.log(recipe);
   res.json({
     status: "success",
     code: 200,
