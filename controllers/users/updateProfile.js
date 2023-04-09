f0c094ecd155f0f4d2ba84e87da41c3737f52cc0
AlexLupeka <lupeka.alex@gmail.com>
2023-04-09 20:27:42 +0300

    was add new file controller updateProfile

---
+++ controllers/users/updateProfile.js
@@ -0,0 +1,50 @@
+const cloudinary = require("cloudinary").v2;
+const { User } = require("../../models");
+const { HttpError } = require("../../helpers");
+
+const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_KEY,
+  api_secret: CLOUDINARY_SECRET,
+});
+
+const updateProfile = async (req, res) => {
+  const { _id } = req.user;
+
+  try {
+    let avatarURL = null;
+    if (req.file) {
+      const updateResult = await cloudinary.uploader.upload(req.file.path, {
+        folder: "avatars",
+      });
+      avatarURL = updateResult.secure_url;
+    }
+
+    let name = null;
+    if (req.body.name) {
+      name = req.body.name;
+    }
+
+    const updatedFields = {};
+    if (avatarURL) {
+      updatedFields.avatarURL = avatarURL;
+    }
+    if (name) {
+      updatedFields.name = name;
+    }
+
+    await User.findByIdAndUpdate(_id, updatedFields, { new: true });
+    const updatedUser = await User.findById(_id);
+    res.json(updatedUser);
+  } catch (error) {
+    throw HttpError(500, "Failed to update profile!");
+  } finally {
+    if (req.file) {
+      cloudinary.uploader.destroy(req.file.path);
+    }
+  }
+};
+
+module.exports = updateProfile;
