aec04f5640d1fcebd5afb3b082857878753826cf
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-10 22:41:28 +0300

    Update recipes.js

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -14,7 +14,7 @@ router.get("/:category", ctrlWrraper(ctrl.recipesCategory));
 router.get("/description/:id", ctrlWrraper(ctrl.recipeById));
 router.get("/search/title", ctrlWrraper(ctrl.searchList));
 router.post("/", auth, uploadCloud.single("img"), ctrlWrraper(ctrl.addRecipe));
-router.delete("/:id", ctrlWrraper(ctrl.deleteRecipeById));
+router.delete("/:id", auth, ctrlWrraper(ctrl.deleteRecipeById));
 router.get("/", auth, ctrlWrraper(ctrl.getAddedRecipes));
 
 module.exports = router;
