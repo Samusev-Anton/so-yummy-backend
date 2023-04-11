bf4badfd1f39ecc5084fec03212968738278306c
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-11 16:23:05 +0300

    Update signUp.js

--- controllers/users/signUp.js
+++ controllers/users/signUp.js
@@ -2,31 +2,37 @@ const { User } = require("../../models");
 const { Conflict } = require("http-errors");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
 
 const signUp = async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.findOne({ email });
+  const payload = {
+    id: uuidv4,
+  };
+
   if (user) {
     throw new Conflict(`User with ${email} already exist`);
   }
   const avatarURL = gravatar.url(email);
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+
   const result = await User.create({
     name,
     email,
     password: hashPassword,
     avatarURL,
+    token,
   });
 
   res.status(201).json({
     status: "success",
     code: 201,
-    user: {
-      name,
-      email: result.email,
-      subscription: result.subscription,
-      avatarURL,
-    },
+    user: result,
   });
 };
 
