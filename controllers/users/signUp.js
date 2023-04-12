a588f799177a8bcec0ea780c17753651e2ba40f0
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-12 08:39:50 +0300

    Update signUp.js

--- controllers/users/signUp.js
+++ controllers/users/signUp.js
@@ -2,7 +2,6 @@ const { User } = require("../../models");
 const { Conflict } = require("http-errors");
 const bcrypt = require("bcryptjs");
 // const gravatar = require("gravatar");
-const { v4: uuidv4 } = require("uuid");
 const jwt = require("jsonwebtoken");
 
 const { SECRET_KEY } = process.env;
@@ -10,24 +9,30 @@ const { SECRET_KEY } = process.env;
 const signUp = async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.findOne({ email });
-  const payload = {
-    id: uuidv4,
-  };
 
   if (user) {
     throw new Conflict(`User with ${email} already exist`);
   }
-  // const avatarURL = gravatar.url(email);
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
 
-  const result = await User.create({
+  const newUser = await User.create({
     name,
     email,
     password: hashPassword,
     // avatarURL,
-    token,
   });
+
+  const payload = {
+    id: newUser._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  console.log(token);
+
+  const result = await User.findByIdAndUpdate(
+    newUser._id,
+    { token },
+    { new: true }
+  );
 
   res.status(201).json({
     status: "success",
