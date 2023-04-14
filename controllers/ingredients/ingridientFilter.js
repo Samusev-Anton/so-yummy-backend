6269432710a7ea58dadc5ce89f3034e7ed214ea5
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-04-14 11:09:12 +0300

    Update ingridientFilter.js

--- controllers/ingredients/ingridientFilter.js
+++ controllers/ingredients/ingridientFilter.js
@@ -2,9 +2,12 @@ const { Ingredient } = require("../../models");
 
 const searchList = async (req, res) => {
   console.log(res.query);
-  const result = await Ingredient.find({
-    ttl: { $regex: req.query.ingredient, $options: "i" },
-  });
+  const result = await Ingredient.find(
+    {
+      ttl: { $regex: req.query.ingredient, $options: "i" },
+    },
+    { new: true }
+  );
 
   res.status(201).json({
     status: "success",
